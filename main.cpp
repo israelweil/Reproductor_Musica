8e158780c6b5b572eb6abdb8be698ab4dcf59e58
ISRAEL <l24120356@morelia.tecnm.mx>
2025-04-27 20:01:56 -0600

    archivos base creados

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
